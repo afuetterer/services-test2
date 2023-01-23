c90fcd36e927ca01fec392f5a32ead66a002b086
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2023-01-23 19:15:45 +0100

    chore: init

---
+++ tests/test_lib.py
@@ -0,0 +1,16 @@
+from services_test import lib
+
+
+def test_hello_world() -> None:
+    input_msg = "hello world"
+    actual = lib.hello_world(input_msg)
+    expected = "HELLO WORLD"
+    assert actual == expected
+
+
+def test_send_message() -> None:
+    instance = lib.World()
+    input_msg = "hello world"
+    actual = instance.send_message(input_msg)
+    expected = "HELLO WORLD"
+    assert actual == expected
---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pytest_mock.plugin import MockerFixture
+
+import services_test
+
+
+def test_entrypoint_calls_app(mocker: "MockerFixture") -> None:
+    mocker.patch("services_test.cli.app")
+    from services_test import __main__  # noqa: F401
+
+    services_test.cli.app.assert_called_once_with(  # type: ignore[attr-defined]
+        prog_name="services-test",
+    )
